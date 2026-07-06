496d3b25bdeae9dd6fdbc2657fc0affc3332061b
dibyendu <dibyendu@manna.com>
2026-07-06 18:20:11 +0530

    Add BCrypt password encryption

--- backend/src/main/java/com/smartlostfound/backend/service/UserService.java
+++ backend/src/main/java/com/smartlostfound/backend/service/UserService.java
@@ -4,6 +4,7 @@ import com.smartlostfound.backend.entity.User;
 import com.smartlostfound.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
@@ -11,7 +12,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User registerUser(User user) {
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return userRepository.save(user);
     }
+
+
 }
